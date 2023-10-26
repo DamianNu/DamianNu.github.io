69ff383be69f150c3f2423bda52422817d5f84f0
DamianNu <damian21v@gmail.com>
2023-10-26 13:24:32 +0200

    new

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'tasks-1.damiannu.repl.co/v1/tasks';
-  const trelloApiRoot = 'https://trello.com/b/6DjuQjyr/kodilla-aplication';
+  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
